0ad9f9531ee9918c4c310263217295846f655479
moudjane <mathis.oudjane@gmail.com>
2024-11-14 00:05:59 +0100

    first commit

---
+++ src/main/java/com/moudjane/birthdayjava/BirthdayjavaApplication.java
@@ -0,0 +1,13 @@
+package com.moudjane.birthdayjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdayjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moudjane/birthdayjava/BirthdayjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moudjane.birthdayjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
